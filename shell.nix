807afe6994e12b258eef9fd7be974e93630cbe23
SzGula <szymon.gula@helsing.ai>
2023-03-30 11:57:34 +0100

    Working on both Linux and Mac (issue, using old cryptography version)

--- shell.nix
+++ shell.nix
@@ -4,7 +4,14 @@ pkgs.mkShell {
 
   buildInputs = [
     pkgs.python310
+    pkgs.python310.pkgs.flake8
     pkgs.poetry
+    pkgs.protobuf
+    #pkgs.black
+    #pkgs.mypy
+    #pkgs.isort
+    pkgs.cargo
+    pkgs.rustfmt
   ];
 
 }
